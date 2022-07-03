e0fc576760b6ff8096594f22852086d7441b5f2c
Suraj <yadavsurajkumar665@gmail.com>
2022-07-03 20:35:11 +0530

    stl started

---
+++ playlist/Striver - youtube/stl/lec1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+// It wil all header files of c++
+// Use this is coding marathon and not in coding
+// interviews or colleges
+
+using namespace std;
+
+namespace raj
+{
+    int val = 50;
+}
+
+int main()
+{
+    // int val = 50;
+    double val = 10.1;
+    cout << val << endl;
+    cout << raj::val << endl;
+    return 0;
+}
